5a06a3191916a6e36d9bbc0f6a35bb8c5100191e
E00322 <hangy@birentech.com>
2021-11-10 09:59:12 +0800

    rm useless files

--- 005_sudnn_register_mechanism/src/api/convfwd_api.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-#include "database/database.h"
-
-int ConvFwdCrossDirect(int a) {
-    return DataBase::GetOperatorDatabase()->operate(
-        std::string("Convfwd"), std::string("Cross"), std::string("Direct"));
-}
--- 005_sudnn_register_mechanism/src/inc/api.h
+++
@@ -1,1 +0,0 @@
-int ConvFwdCrossDirect(int a);
